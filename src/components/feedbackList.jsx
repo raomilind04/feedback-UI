253284b37ccd34abaaff91e45c299d72e8e6611a
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-07-22 14:23:51 +0530

    implement animation

--- src/components/feedbackList.jsx
+++ src/components/feedbackList.jsx
@@ -1,15 +1,20 @@
 import FeedBackItem from "./feedbackItem";
 import propTypes from "prop-types";
+import {motion, AnimatePresence} from "framer-motion"; 
 
 function FeedbackList({ feedback, handleDelete }) {
   console.log(feedback);
   return (
     <div className="feedback-list">
+    <AnimatePresence>
       {feedback.map((item, index) => {
         return (
+          <motion.div key= {item.id} initial= {{opacity: 0}} animate= {{opacity: 1}} exit= {{opacity: 0}}>
           <FeedBackItem key={item.id} item={item} handleDelete={handleDelete} />
+          </motion.div>
         );
       })}
+      </AnimatePresence>
     </div>
   );
 }
